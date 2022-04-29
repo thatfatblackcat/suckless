ac9622adbdf574e2ae391550e52dadbfffd6b999
thatfatblackcat <thatfatblackcat@protonmail.com>
2022-04-29 17:59:05 +0200

    Shuffled keybinds around

--- source/config.h
+++ source/config.h
@@ -32,13 +32,6 @@ static const char *dmenucmd[] = {
 	"-sf", dmenu_sf, NULL
 };
 
-static const char *nsxivcmd[] = {
-	"sxiv", "-ro",
-	"artworks",
-	"tatenaga",
-	"yokonaga", NULL
-};
-
 static const char *tags[] = {
 	"1", "2", "3",
 	"4", "5", "6",
@@ -74,49 +67,47 @@ static const Rule rules[] = {
 };
 
 static Key keys[] = {
+
+	{ MODKEY, XK_0, view, {.ui = ~0 } },
+	{ MODKEY|ShiftMask, XK_0, tag, {.ui = ~0 } },
+	{ MODKEY, XK_p, spawn, {.v = dmenucmd } },
+	{ MODKEY|ShiftMask, XK_p, spawn, {.v = spawncmd } },
+	{ MODKEY, XK_q, killclient, {0} },
+	{ MODKEY|ShiftMask, XK_q, quit, {0} },
+
 	TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2)
 	TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5)
 	TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7) TAGKEYS(XK_9, 8)
 
+	{ MODKEY, XK_h, setmfact, {.f = -0.05} },
+	{ MODKEY|ShiftMask, XK_h, incnmaster, {.i = -1 } },
+	{ MODKEY, XK_j, focusstack, {.i = +1 } },
+	{ MODKEY|ShiftMask, XK_j, movestack, {.i = +1 } },
+	{ MODKEY, XK_k, focusstack, {.i = -1 } },
+	{ MODKEY|ShiftMask, XK_k, movestack, {.i = -1 } },
+	{ MODKEY, XK_l, setmfact, {.f = +0.05} },
+	{ MODKEY|ShiftMask, XK_l, incnmaster, {.i = +1 } },
+
+	{ MODKEY, XK_t, setlayout, {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask, XK_t, setlayout, {.v = &layouts[1]} },
+	{ MODKEY, XK_f, setlayout, {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask, XK_f, setlayout, {.v = &layouts[3]} },
+	{ MODKEY, XK_r, setlayout, {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask, XK_r, setlayout, {.v = &layouts[5]} },
+
 	{ 0, XF86XK_AudioMicMute, spawn, SHCMD("pactl set-source-mute @DEFAULT_SOURCE@ toggle") },
 	{ 0, XF86XK_AudioMute, spawn, SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle") },
 
-	{ 0, XF86XK_AudioLowerVolume, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -1000") },
 	{ 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +1000") },
-
-	{ MODKEY, XK_j, focusstack, {.i = +1 } },
-	{ MODKEY, XK_k, focusstack, {.i = -1 } },
-	{ MODKEY|ShiftMask, XK_j, movestack, {.i = +1 } },
-	{ MODKEY|ShiftMask, XK_k, movestack, {.i = -1 } },
-
-	{ MODKEY, XK_h, setmfact, {.f = -0.05} },
-	{ MODKEY, XK_l, setmfact, {.f = +0.05} },
-	{ MODKEY|ShiftMask, XK_h, incnmaster, {.i = -1 } },
-	{ MODKEY|ShiftMask, XK_l, incnmaster, {.i = +1 } },
-
-	{ MODKEY, XK_f, setlayout, {.v = &layouts[2]} },
-	{ MODKEY, XK_r, setlayout, {.v = &layouts[4]} },
-	{ MODKEY, XK_t, setlayout, {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask, XK_f, setlayout, {.v = &layouts[3]} },
-	{ MODKEY|ShiftMask, XK_r, setlayout, {.v = &layouts[5]} },
-	{ MODKEY|ShiftMask, XK_t, setlayout, {.v = &layouts[1]} },
+	{ 0, XF86XK_AudioLowerVolume, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -1000") },
 
 	{ MODKEY, XK_comma, focusmon, {.i = -1 } },
-	{ MODKEY, XK_period, focusmon, {.i = +1 } },
 	{ MODKEY|ShiftMask, XK_comma, tagmon, {.i = -1 } },
+	{ MODKEY, XK_period, focusmon, {.i = +1 } },
 	{ MODKEY|ShiftMask, XK_period, tagmon, {.i = +1 } },
 
-	{ MODKEY, XK_q, killclient, {0} },
-	{ MODKEY|ShiftMask, XK_q, quit, {0} },
-
-	{ MODKEY, XK_p, spawn, {.v = dmenucmd } },
-	{ MODKEY|ShiftMask, XK_p, spawn, {.v = spawncmd } },
-
-	{ MODKEY, XK_s, spawn, {.v = nsxivcmd } },
-	{ MODKEY|ShiftMask, XK_s, spawn, {.v = nsxivcmd } },
-
-	{ MODKEY, XK_0, view, {.ui = ~0 } },
-	{ MODKEY|ShiftMask, XK_0, tag, {.ui = ~0 } },
+	{ MODKEY, XK_s, spawn, SHCMD("sxiv -ro artworks/ tatenaga/ yokonaga/") },
+	{ MODKEY|ShiftMask, XK_s, spawn, SHCMD("nsxiv -ro artworks/ tatenaga/ yokonaga/") },
 
 	{ MODKEY, XK_space, togglebar, {0} },
 	{ MODKEY|ShiftMask, XK_space, togglefloating, {0} },
