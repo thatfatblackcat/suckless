7a2ae77587ccc978042db45bfea4c1dbabe060b9
thatfatblackcat <thatfatblackcat@protonmail.com>
2022-04-29 09:59:28 +0200

    Added back togglefloating function

--- source/config.h
+++ source/config.h
@@ -108,8 +108,8 @@ static Key keys[] = {
 	{ MODKEY, XK_0, view, {.ui = ~0 } },
 	{ MODKEY|ShiftMask, XK_0, tag, {.ui = ~0 } },
 
-	{ MODKEY, XK_space, setlayout, {0} },
-	{ MODKEY|ShiftMask, XK_space, togglebar, {0} },
+	{ MODKEY, XK_space, togglebar, {0} },
+	{ MODKEY|ShiftMask, XK_space, togglefloating, {0} },
 
 	{ MODKEY, XK_Return, zoom, {0} },
 	{ MODKEY|ShiftMask, XK_Return, view, {0} },
